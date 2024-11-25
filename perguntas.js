f650b86f3b3ddfdf5a63a84faf5ad55a1996edea
MariaGaby2007 <gabrie77y@gmail.com>
2024-11-25 15:42:49 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Bea',
+    'Quais jogos a Bea mais joga?',
+    'Stardewvalley, Stumble Guys e Minecraft'
+)
+
+criaCartao(
+    'Ryan',
+    'O que o Ryan não come?',
+    'Carne de porco e chocolate'
+)
+
+criaCartao(
+    'Grupo',
+    'Qual o apelido de todos do grupo?(Gaby, Ryan, Adrian e Bea)',
+    'Doido'
+)
+
+criaCartao(
+    'Stumble guys',
+    'Qual askin mais legal que a Gaby possui?',
+    'Emojutsu (mítico)'
+)
